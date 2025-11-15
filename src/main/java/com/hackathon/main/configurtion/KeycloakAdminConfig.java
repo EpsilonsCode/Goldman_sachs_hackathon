02e2df1c02cc68acf2d0a67f9a971d71b4ebe335
stefanialukaszewska <stefania.lukaszewska@gmail.com>
2025-11-15 07:44:11 +0100

    dont use this

--- src/main/java/com/hackathon/main/configurtion/KeycloakAdminConfig.java
+++ src/main/java/com/hackathon/main/configurtion/KeycloakAdminConfig.java
@@ -4,16 +4,21 @@ package com.hackathon.main.configurtion;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value; // Importuj
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KeycloakAdminConfig {
 
+    // Wstrzyknij wartość z application.properties
+    @Value("${keycloak.server-url}")
+    private String serverUrl;
+
     @Bean
     public Keycloak keycloakAdmin() {
         return KeycloakBuilder.builder()
-                .serverUrl("http://localhost:8180")
+                .serverUrl(serverUrl) // Użyj wstrzykniętej wartości
                 .realm("master")
                 .username("admin")
                 .password("admin")
@@ -22,4 +27,3 @@ public class KeycloakAdminConfig {
                 .build();
     }
 }
-
