220fda3fc198af8f74e1893f37d69c7d1015cc5c
EpsilonsCode <michal.lutomirskii@gmail.com>
2025-11-15 08:15:56 +0100

    Scoring final

--- src/main/java/com/hackathon/main/service/ScoringService.java
+++ src/main/java/com/hackathon/main/service/ScoringService.java
@@ -1,46 +1,38 @@
 package com.hackathon.main.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hackathon.main.model.Task;
 import com.hackathon.main.model.TaskFile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Arrays;
-import java.util.Base64;
+import java.util.*;
 
-import java.io.IOException;
 import java.util.Base64;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ScoringService {
     private final TaskService taskService;
-    public int calculateScore(String taskId, MultipartFile file) throws IOException {
-        // TODO: Zaimplementować logikę walidacji i oceniania
 
+    public int calculateScore(String taskId, MultipartFile file) throws Exception {
         TaskFile taskFile = taskService.getTaskById(taskId).getSolutionFile();
         byte[] bytes = Base64.getDecoder().decode(taskFile.getDataBase64());
 
-        String taskRightSolution = new String(bytes);;
-        String taskSolution = new String(file.getBytes());
-        ObjectMapper mapper = new ObjectMapper();
+        String correctSolution = new String(bytes);
+        String submittedSolution = new String(file.getBytes());
 
-        List<Integer> correct = fileToList(taskRightSolution, taskFile.getContentType());
-        List<Integer> submitted = fileToList(taskSolution, file.getContentType());
+
+        List<Integer> correct = fileToList(correctSolution, taskFile.getContentType());
+        List<Integer> submitted = fileToList(submittedSolution, file.getContentType());
+        if (correct.size() != submitted.size())
+            throw new Exception();
         double output = rmse(correct, submitted);
-
-        int score = (int) Math.max(0, 100 - output * 10);
+        int score = (int) Math.max(0, 100 - output * 3);
         return score;
     }
 
-    private List<Integer> fileToList(String content, String type) throws JsonProcessingException {
+    private static List<Integer> fileToList(String content, String type) throws Exception {
 
-        ObjectMapper mapper = new ObjectMapper();
 
         // 1. JSON ARRAY → must start with "[" and end with "]"
         if (type != null && type.equals("application/json")) {
@@ -49,14 +41,24 @@ public class ScoringService {
             }
 
             try {
-                return mapper.readValue(content, new TypeReference<List<Integer>>() {});
+                String inner = content.substring(1, content.length() - 1).trim();
+
+                if (inner.isEmpty()) return List.of();
+
+                String[] parts = inner.split(",");
+
+                List<Integer> result = new ArrayList<>();
+                for (String part : parts) {
+                    result.add(Integer.parseInt(part.trim()));
+                }
+                return result;
             } catch (Exception e) {
                 throw new IllegalArgumentException("Failed to parse JSON array", e);
             }
         }
 
         // 2. CSV ARRAY → no brackets, contains commas
-        if (type != null && (type.equals("text/csv") || type.equals("text/plain"))) {
+        if (type != null && (type.equals("text/csv"))) {
 
             if (!content.contains(",")) {
                 throw new IllegalArgumentException("Invalid CSV array format");
@@ -76,7 +78,7 @@ public class ScoringService {
         throw new IllegalArgumentException("Unsupported file type: " + type);
     }
 
-    private double rmse(List<Integer> a, List<Integer> b) {
+    private static double rmse(List<Integer> a, List<Integer> b) {
         if (a.size() != b.size()) {
             throw new IllegalArgumentException("Arrays must have the same length");
         }
